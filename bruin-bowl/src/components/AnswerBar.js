1f373ffedea8168f29b12647f0ab0751335b62d2
DavidSai1 <davidsai342@gmail.com>
2024-05-06 22:24:23 -0700

    Add answer bar component
    Add component for the answer bar, delete boilerplate in App.js

---
+++ bruin-bowl/src/components/AnswerBar.js
@@ -0,0 +1,12 @@
+function AnswerBar() {
+    return (
+        <>
+            <form>
+                <input type="text" id="answer"></input>
+                <input type="submit" value="Submit"></input>
+            </form>
+        </>
+    );
+}
+
+export default AnswerBar;
