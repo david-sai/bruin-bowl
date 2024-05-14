ad0c4873196151da3e8ce274d9d3282b694fc253
zacharydesoto <zacharysdesoto@gmail.com>
2024-05-13 23:49:37 -0700

    Add Multiple Choice
    Added multiple choice and a correct answer state stored in App.js

--- bruin-bowl/src/App.js
+++ bruin-bowl/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import AnswerBar from './components/AnswerBar.js';
 import PageTitle from './components/PageTitle.js';
 import QuestionBox from './components/QuestionBox.js';
 
 function App() {
+  const [correct, setCorrect] = useState(false); // Player guessed correct answer
+
   return (
     <div>
       <>
         <PageTitle title="BruinBowl" />
         <QuestionBox />
-        <AnswerBar />
+        <AnswerBar setVar={setCorrect} />
       </>
     </div>
   );
