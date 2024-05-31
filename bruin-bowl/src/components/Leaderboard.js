b618840636bbd79aa3ed83a86f5c521593a3126a
Daniel Guo <dguo314other@gmail.com>
2024-05-31 16:10:35 -0700

    Delete unnecessary code in Leaderboard.js and ProfileStyles.css
    Deleted commented out code in Leaderboard.js component and ProfileStyles.css

--- bruin-bowl/src/components/Leaderboard.js
+++ bruin-bowl/src/components/Leaderboard.js
@@ -14,19 +14,3 @@ export default function Leaderboard () {
 
 }
 
-// function between(data) {
-//     // Potential feature: show scores up to a certain date
-//     let filter = data.filter(val => {
-//         return true;
-//     })
-
-//     // Sort scores by ascending order
-//     return filter.sort((a, b) => {
-//         if(a.score === b.score) {
-//             return b.score - a.score;
-//         } else {
-//             return b.score - a.score;
-//         }
-//     })
-// }
-
