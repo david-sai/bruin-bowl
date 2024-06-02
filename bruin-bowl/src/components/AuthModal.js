3713fec06b94819167fd2166336c5872c83bf32f
Andrew Zheng <aheze@getfind.app>
2024-06-01 17:50:55 -0700

    Fix checks

--- bruin-bowl/src/components/AuthModal.js
+++ bruin-bowl/src/components/AuthModal.js
@@ -5,50 +5,64 @@ import { useState, useContext } from "react";
 import { ModalIsOpenContext } from "../context/Contexts.js";
 
 import { signup } from "../api/api.js";
+import { signin } from "../api/api.js";
 
 function AuthModal() {
   const { modalIsOpen, setModalIsOpen } = useContext(ModalIsOpenContext);
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
   const [error, setError] = useState("");
   const [isSignIn, setIsSignIn] = useState(false);
 
+  const [isLoading, setIsLoading] = useState(false);
   const [successfullySignedUp, setSuccessfullySignedUp] = useState(false);
   const [successfullySignedIn, setSuccessfullySignedIn] = useState(false);
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
   const handleSubmit = () => {
-    // Call the function when the button is clicked
-    const response = (data) => {
-      if (username == "") {
-        setError("Username is empty.");
-        return;
-      }
+    if (isLoading) {
+      console.log("is loagin!!");
+      return;
+    }
 
-      if (password == "") {
-        setError("Password is empty.");
-        return;
-      }
+    if (username == "") {
+      setError("Username is empty.");
+      return;
+    }
 
+    if (password == "") {
+      setError("Password is empty.");
+      return;
+    }
+
+    if (!isSignIn) {
       // username must only contain letters, numbers, and underscores
-      if (!/^[A-Za-z0-9_]*$/.test(username)|| username.length < 4) {
-        setError("Username must be at least 4 characters long and can only contain letters, numbers, and underscores.");
+      if (!/^[A-Za-z0-9_]*$/.test(username) || username.length < 4) {
+        setError(
+          "Username must be at least 4 characters long and can only contain letters, numbers, and underscores."
+        );
         return;
       }
 
       // password must be at least 8 characters long and only contain letters and numbers
       if (!/^[A-Za-z0-9]*$/.test(password) || password.length < 4) {
-        setError("Password must be at least 4 characters long and only contain letters and numbers.");
+        setError(
+          "Password must be at least 4 characters long and only contain letters and numbers."
+        );
         return;
       }
+
+      // make sure passwords match
+      if (password != password2) {
+        setError("Passwords do not match.");
+        return;
+      }
+    }
+
+    // Call the function when the button is clicked
+    const response = (data) => {
+      setIsLoading(false);
 
       if (data) {
         if (data["error"]) {
@@ -62,9 +76,10 @@ function AuthModal() {
       }
     };
 
+    setIsLoading(true);
 
     if (isSignIn) {
-      // signin(username, password, response);
+      signin(username, password, response);
     } else {
       signup(username, password, response);
     }
@@ -73,11 +88,11 @@ function AuthModal() {
   const customStyles = {
     content: {
       maxWidth: "400px",
-      maxHeight: "450px",
+      maxHeight: "500px",
       margin: "auto", // center horizontally
       backgroundColor: "#fffbeb",
       borderRadius: "24px",
-      borderColor: "#f0e68c"
+      borderColor: "#f0e68c",
     },
   };
   return (
@@ -103,7 +118,7 @@ function AuthModal() {
             <input
               type="text"
               value={username}
-              onChange={handleUsernameChange}
+              onChange={(e) => setUsername(e.target.value)}
               placeholder="Enter username"
               className="p-3 w-full bg-transparent border rounded-md border-bruin-gold"
             />
@@ -111,10 +126,20 @@ function AuthModal() {
             <input
               type="password"
               value={password}
-              onChange={handlePasswordChange}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="Enter password"
               className="p-3 w-full bg-transparent border rounded-md border-bruin-gold"
             />
+
+            {!isSignIn && (
+              <input
+                type="password"
+                value={password2}
+                onChange={(e) => setPassword2(e.target.value)}
+                placeholder="Confirm password"
+                className="p-3 w-full bg-transparent border rounded-md border-bruin-gold"
+              />
+            )}
           </div>
 
           <button
