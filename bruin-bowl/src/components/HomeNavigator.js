4ca170903d7778323d7172fc41ff44b5e0fd8ffd
zacharydesoto <zacharysdesoto@gmail.com>
2024-06-01 19:30:15 -0700

    Change Home Page Button Styling

--- bruin-bowl/src/components/HomeNavigator.js
+++ bruin-bowl/src/components/HomeNavigator.js
@@ -6,15 +6,19 @@ function HomeNavigator() {
 
     if (isSignedIn) {
         return (
-            <div className="text-bruin-blue font-bold text-2xl">
-                <Link to={"/mode-select"}>Start playing</Link>
+            <div >
+                <button className="mt-1 px-4 py-2 bg-bruin-blue text-white text-xl font-bold rounded-full">
+                    <Link to='/mode-select'>Start playing</Link>
+                </button>
             </div>
         )
     }
     else {
         return (
             <div className="text-bruin-blue font-bold text-2xl">
-                <Link to='/sign-in'>Sign in</Link>
+                <button className="mt-1 px-4 py-2 bg-bruin-blue text-white text-xl font-bold rounded-full">
+                    <Link to='/sign-in'>Sign in</Link>
+                </button>
             </div>
         )
     }
