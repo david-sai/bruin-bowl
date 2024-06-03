1875f3be33c069716499a349d2eb31de833dded9
Andrew Zheng <aheze@getfind.app>
2024-06-03 15:51:51 -0700

    Work on add questions

--- bruin-bowl/src/components/AddQuestions.js
+++ bruin-bowl/src/components/AddQuestions.js
@@ -1,17 +1,16 @@
-import React, { useState } from 'react';
-import { createQuestion } from '../api/api.js';
-import { CATEGORIES } from '../context/GameContext'
+import React, { useState } from "react";
+import { createQuestion } from "../api/api.js";
+import { CATEGORIES } from "../context/GameContext";
 
 function AddQuestions() {
-  const [question, setQuestion] = useState('');
-  const [answer, setAnswer] = useState('');
-  const [wrong1, setWrong1] = useState('');
-  const [wrong2, setWrong2] = useState('');
-  const [wrong3, setWrong3] = useState('');
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+  const [wrong1, setWrong1] = useState("");
+  const [wrong2, setWrong2] = useState("");
+  const [wrong3, setWrong3] = useState("");
   const [results, setResults] = useState(null);
   const [error, setError] = useState("");
   const categoryStrings = Object.values(CATEGORIES);
-
 
   const handleChangeQuestion = (event) => {
     setQuestion(event.target.value);
@@ -37,14 +36,26 @@ function AddQuestions() {
     const formData = new FormData(form);
     const formJson = Object.fromEntries(formData.entries());
 
+    // check if question, answer, and wrong answers are empty
+    if (
+      question === "" ||
+      answer === "" ||
+      wrong1 === "" ||
+      wrong2 === "" ||
+      wrong3 === ""
+    ) {
+      setError("Please fill out all fields.");
+      return;
+    }
+
     const jsonData = {
       question,
       answer,
       option1: wrong1,
       option2: wrong2,
       option3: wrong3,
-      category: formJson.category
-    }
+      category: formJson.category,
+    };
 
     const response = (data) => {
       if (data) {
@@ -52,14 +63,18 @@ function AddQuestions() {
           setError(data["error"].message);
         } else {
           console.log(data);
-          setResults(data["results"])
+
+          const r = data["quizQuestion"];
+          setResults(r);
           setError("");
+
+          console.log(r.answer);
         }
       }
     };
 
     createQuestion(jsonData, response);
-  }
+  };
 
   return (
     <div>
@@ -114,15 +129,31 @@ function AddQuestions() {
       <br></br>
       <h2 className="text-xl mb-1.5">Category</h2>
       <form method="post" onSubmit={handleSubmit}>
-        {categoryStrings.map((category, index) => { // Creates a button for each category
+        {categoryStrings.map((category, index) => {
+          // Creates a button for each category
           return (
             <div className="mb-1">
-              <input type="radio" id={category} name="category" value={category} defaultChecked={index === 0} />
-              <label htmlFor={category} className="ml-2">{category}</label>
+              <input
+                type="radio"
+                id={category}
+                name="category"
+                value={category}
+                defaultChecked={index === 0}
+              />
+              <label htmlFor={category} className="ml-2">
+                {category}
+              </label>
             </div>
           );
         })}
-        <button type="submit" className="mt-4 px-4 py-2 bg-bruin-gold text-white rounded-full">Submit</button>
+        <button
+          type="submit"
+          className="mt-4 px-4 py-2 bg-bruin-gold text-white rounded-full"
+        >
+          Submit
+        </button>
+
+        {error && <p className="text-red-500 mt-4">{error}</p>}
       </form>
     </div>
   );
