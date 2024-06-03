4cd7e27f8c2fa40d6d5ee3fbc972df2a610815f5
Andrew Zheng <aheze@getfind.app>
2024-06-02 19:00:10 -0700

    Polish up leaderboard

--- bruin-bowl/src/components/LeaderboardDisplay.js
+++ bruin-bowl/src/components/LeaderboardDisplay.js
@@ -1,32 +1,50 @@
-import React, {useEffect, useState} from 'react';
-import Profiles from "./Profiles";
-import { getLeaderBoard } from '../api/api';
+import React, { useEffect, useState } from "react";
+import { getLeaderBoard } from "../api/api";
 
-function LeaderboardDisplay () {
-    const [leaderboard, setLeaderboard ] = useState(null)
-    const [error, setError ] = useState("")
+function LeaderboardDisplay() {
+  const [leaderboard, setLeaderboard] = useState(null);
+  const [error, setError] = useState("");
 
-    useEffect(() => {
-        const response = (data) => {
-          if (data) {
-            if (data["error"]) {
-              setError(data["error"].message);
-            } else {
-              console.log(data);
-              setLeaderboard(data["usernames"]);
-              setError("");
-            }
-          }
-        };
-        getLeaderBoard(response);
-      }, []);
+  useEffect(() => {
+    const response = (data) => {
+      if (data) {
+        if (data["error"]) {
+          setError(data["error"].message);
+        } else {
+          console.log(data);
+          setLeaderboard(data["usernames"]);
+          setError("");
+        }
+      }
+    };
+    getLeaderBoard(response);
+  }, []);
 
-    return (
-        <div className="board">
-            {leaderboard && <Profiles LeaderboardData={leaderboard}></Profiles>}
-        </div>
-    )
+  return (
+    <div className="board">
+      {leaderboard &&
+        leaderboard.map((value, index) => (
+          <div
+            key={index}
+            className="flex items-center justify-between bg-amber-50 py-6 px-12 rounded-3xl mb-4"
+          >
+            <div className="flex items-center ">
+              <p className="font-bold text-3xl mr-8">#{index + 1}</p>
 
+              <img
+                className="w-16 h-16 rounded-full object-cover mr-6 shadow-md"
+                src={value.avatar}
+                alt=""
+              />
+              <h3 className="">{value.username}</h3>
+            </div>
+            <div>
+              <span>{value.score}</span>
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 }
 
 export default LeaderboardDisplay;
--- bruin-bowl/src/pages/Leaderboard.js
+++ bruin-bowl/src/pages/Leaderboard.js
@@ -4,7 +4,7 @@ import LeaderboardDisplay from "../components/LeaderboardDisplay.js";
 function Leaderboard() {
     return (
         <div className="mt-4 bg-yellow-600 bg-opacity-5 rounded-3xl p-10 text-bruin-darkgold">
-        <h1 className="font-bold text-3xl mb-1.5">Leaderboard</h1>
+        <h1 className="font-bold text-3xl mb-10">Leaderboard</h1>
         <p className="text-2xl">
             <LeaderboardDisplay />
         </p>
