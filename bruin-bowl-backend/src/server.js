f6b9d593ca288f90eb24a72e76296666ed5cd01a
Jonathan Pai <jonathantspai@gmail.com>
2024-06-06 04:07:13 -0700

    Cleaned up & added necessary comments to server.js

--- bruin-bowl-backend/src/server.js
+++ bruin-bowl-backend/src/server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const quizRoutes = require("./routes/quizRoutes");
-var cors = require("cors");
+let cors = require("cors");
 require('dotenv').config();
-
 
 const app = express();
 
+//Connect to mongoDB using mongoose
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
@@ -22,16 +22,21 @@ mongoose
     console.log(error);
   });
 
+//Set up middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //Parses the JSON bodies
+
+//Routes
 app.use("/user", userRoutes);
 app.use("/quiz", quizRoutes);
 
+//Logs middeware to output the request methods & paths
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
+//This is the root route which confirms that the server is online
 app.get("/", (req, res) => res.json("BruinBowl Server Online"));
 
 module.exports = app;
