0f4ea64964cb58e56986d682d42e2f21433e68de
Jonathan Pai <jonathantspai@gmail.com>
2024-06-06 03:52:57 -0700

    Refactored and commented quizRoutes.js

--- bruin-bowl-backend/src/routes/quizRoutes.js
+++ bruin-bowl-backend/src/routes/quizRoutes.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require("express"); //Creates router instances
 
-const { createQuestion, getQuestion, searchQuestion } = require("../controllers/quizController");
+const { createQuestion, getQuestion, searchQuestion } = require("../controllers/quizController"); //Quiz-related controller fns
 
-const router = express.Router();
+const router = express.Router(); //Defines routes for quiz operations
 
 router.post("/create", createQuestion);
+
 router.post("/search", searchQuestion);
+
 router.get("", getQuestion);
 
-
-module.exports = router;
+module.exports = router; //Export the quiz router to be mounted by the main app.
