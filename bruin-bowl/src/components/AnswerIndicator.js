6d871e6458e8e664a8f7fbcc440d5c8ff2dc0276
Brayden Tam <57848434+braydentam@users.noreply.github.com>
2024-06-06 00:51:47 -0700

    score generation based on speed and mode

--- bruin-bowl/src/components/AnswerIndicator.js
+++ bruin-bowl/src/components/AnswerIndicator.js
@@ -3,10 +3,11 @@ import { STATUS } from '../pages/Questions'
 import { UserContext } from "../context/Contexts";
 import { useState, useContext, useEffect } from "react"
 import { updateScore } from '../api/api';
-
+import { GameStateContext } from '../context/GameContext';
 
 function AnswerIndicator({ status, answer, score, setScore }) {
     const { user, setUser } = useContext(UserContext);
+    const state = useContext(GameStateContext);
     const [error, setError] = useState("")
 
     useEffect(() => {
@@ -24,9 +25,20 @@ function AnswerIndicator({ status, answer, score, setScore }) {
       };
 
       if(status == STATUS.CORRECT_ANSWER){
-        setUser({ ...user, score: user.score + 10 });
-        setScore(score + 10);
-        updateScore(user.username, 10, response);
+        var updatedScore = 0;
+        if(state.gameMode == "Rapid"){
+          updatedScore = (state.timeRemaining + 5) * 1.5;
+        }
+        else if(state.gameMode == "Blitz"){
+          updatedScore = (state.timeRemaining + 10) * 3;
+        }
+        else{
+          updatedScore = state.timeRemaining;
+        }
+        console.log(updatedScore)
+        setUser({ ...user, score: user.score + updatedScore});
+        setScore(score + updatedScore);
+        updateScore(user.username, updatedScore, response);
       }
     }, [status]);
 
