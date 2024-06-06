f123f7ca83355c8064f70ba578ee1a3fa13f6694
zacharydesoto <zacharysdesoto@gmail.com>
2024-06-05 23:18:14 -0700

    Refactor Leaderboard
    Switched p to div to remove warning

--- bruin-bowl/src/pages/Leaderboard.js
+++ bruin-bowl/src/pages/Leaderboard.js
@@ -5,9 +5,9 @@ function Leaderboard() {
     return (
         <div className="mt-4 bg-yellow-600 bg-opacity-5 rounded-3xl p-10 text-bruin-darkgold">
         <h1 className="font-bold text-3xl mb-10">Leaderboard</h1>
-        <p className="text-2xl">
+        <div className="text-2xl">
             <LeaderboardDisplay />
-        </p>
+        </div>
         </div>
     );
 }
